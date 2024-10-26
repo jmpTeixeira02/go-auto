c41af1f430757b7e394b1312f827aae4b043468e
Teixeira <51384667+jmpTeixeira02@users.noreply.github.com>
2024-10-26 14:39:03 +0100

    feat: scrapper car to data

---
+++ data/data.go
@@ -0,0 +1,56 @@
+package data
+
+import (
+	"go-auto/scrapper"
+	"strconv"
+	"strings"
+)
+
+type Car struct {
+	Car          string
+	Price        int
+	Mileage      int
+	Fuel         string
+	Year         int
+	Hp           int
+	Displacement int
+	Link         string
+}
+
+func separateHpDisplace(str string) []string {
+	return strings.Split(str, "•")
+}
+
+func stringToInt(str string) int {
+	res, err := strconv.Atoi(strings.ReplaceAll(str, " ", ""))
+	if err != nil {
+		panic(err)
+	}
+	return res
+}
+
+func CarScrapperToCar(carScrape scrapper.CarScrape) Car {
+	car := Car{
+		Car:     carScrape.Model,
+		Price:   stringToInt(carScrape.Price),
+		Mileage: stringToInt(carScrape.Mileage),
+		Fuel:    carScrape.Fuel,
+		Year:    stringToInt(carScrape.Year),
+		Link:    carScrape.Link,
+	}
+	err := UpdateCarPower(&car, carScrape.Power)
+	if err != nil {
+		panic(err)
+	}
+	return car
+}
+
+func UpdateCarPower(car *Car, raw string) error {
+	res := make([]int, 2)
+	for i, str := range separateHpDisplace(raw) {
+		res[i] = stringToInt(strings.Split(str, "c")[0])
+	}
+	car.Displacement = res[0]
+	car.Hp = res[1]
+	return nil
+}
---
+++ data/data_test.go
@@ -0,0 +1,55 @@
+package data
+
+import (
+	"go-auto/scrapper"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUpdateCarPower(t *testing.T) {
+	// Arrange
+	var actual Car
+	expected := Car{
+		Hp:           100,
+		Displacement: 1342,
+	}
+
+	// Act
+	err := UpdateCarPower(&actual, "1 342 cm3 • 100 cv")
+	if err != nil {
+		panic(err)
+	}
+
+	// Assert
+	assert.EqualValues(t, expected, actual)
+}
+
+func TestCarScrapperToCar(t *testing.T) {
+	// Arrange
+	expected := Car{
+		Car:          "Toyota Yaris",
+		Price:        10000,
+		Mileage:      89123,
+		Fuel:         "Gasolina",
+		Year:         2008,
+		Hp:           100,
+		Displacement: 1342,
+		Link:         "test",
+	}
+	scrapped := scrapper.CarScrape{
+		Model:   "Toyota Yaris",
+		Price:   "10 000",
+		Mileage: "89 123",
+		Fuel:    "Gasolina",
+		Year:    "2008",
+		Power:   "1 342 cm3 • 100 cv",
+		Link:    "test",
+	}
+
+	// Act
+	car := CarScrapperToCar(scrapped)
+
+	// Assert
+	assert.EqualValues(t, expected, car)
+}
