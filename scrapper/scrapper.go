a9d422b717288a1d9dc470350c71ee23a8d43342
Teixeira <51384667+jmpTeixeira02@users.noreply.github.com>
2024-10-26 11:13:22 +0100

    feat: scrape all cards on page

--- scrapper/scrapper.go
+++ scrapper/scrapper.go
@@ -1,12 +1,22 @@
 package scrapper
 
 import (
+	"strings"
+
 	"github.com/gocolly/colly"
 )
 
 const (
-	carCard      = ".ooa-yca59n.epwfahw0"
-	titleSection = ".epwfahw9.ooa-1ed90th.er34gjf0"
+	carCard          = ".ooa-yca59n.epwfahw0"
+	titleSection     = ".epwfahw9.ooa-1ed90th.er34gjf0"
+	carPowerSection  = ".epwfahw10.ooa-1tku07r.er34gjf0"
+	detailsSection   = ".ooa-1uwk9ii.epwfahw11"
+	detail           = ".ooa-1omlbtp.epwfahw13"
+	detailParameter  = "data-parameter"
+	mileageParameter = "mileage"
+	fuelParameter    = "fuel_type"
+	yearParameter    = "first_registration_year"
+	priceSection     = ".epwfahw16.ooa-1n2paoq.er34gjf0"
 )
 
 type Car struct {
@@ -42,21 +52,48 @@ func (s *Scrapper) StartCars(cars *[]Car, opts ...func(*Car, *colly.HTMLElement)
 	})
 }
 
-// Returns a tuple of strings with the model and link for the car
-// Car follows the following format: Brand Model
+// TitleSection contains a title with the brand and model and href to the link
+// Title follows the following format: Brand Model
 func GetCarModel(c *Car, e *colly.HTMLElement) {
 	e.ForEach(titleSection, func(_ int, el *colly.HTMLElement) {
-		c.Model = el.Text
+		c.Model = strings.TrimSpace(el.Text)
 		el.ForEach("a", func(_ int, i_el *colly.HTMLElement) {
 			c.Link = i_el.Attr("href")
 		})
 	})
 }
 
-// Returns a string containing the displacement and hp
+// Power Section has the displacement followed by the horsepower
 // "X XXX cm3 • XXX cv"
 func GetCarPower(c *Car, e *colly.HTMLElement) {
-	e.ForEach(".epwfahw10.ooa-1tku07r.er34gjf0", func(_ int, el *colly.HTMLElement) {
-		c.Power = el.Text
+	e.ForEach(carPowerSection, func(_ int, el *colly.HTMLElement) {
+		c.Power = strings.TrimSpace(el.Text)
+	})
+}
+
+// Details section contains the mileage, fuelType and year
+// each of which it's identified by a detailParameter
+func GetCarDetails(c *Car, e *colly.HTMLElement) {
+	// Details Section
+	e.ForEach(detailsSection, func(_ int, el *colly.HTMLElement) {
+		// Detail
+		e.ForEach(detail, func(_ int, i_el *colly.HTMLElement) {
+			dataParameter := i_el.Attr(detailParameter)
+			switch dataParameter {
+			case mileageParameter:
+				c.Mileage = strings.TrimSpace(i_el.Text)
+			case fuelParameter:
+				c.Fuel = strings.TrimSpace(i_el.Text)
+			case yearParameter:
+				c.Year = strings.TrimSpace(i_el.Text)
+			}
+		})
+	})
+}
+
+func GetCarPrice(c *Car, e *colly.HTMLElement) {
+	// Price
+	e.ForEach(priceSection, func(_ int, el *colly.HTMLElement) {
+		c.Price = strings.TrimSpace(el.Text)
 	})
 }
--- scrapper/scrapper_test.go
+++ scrapper/scrapper_test.go
@@ -49,6 +49,7 @@ func TestGetCarModel(t *testing.T) {
 	}
 
 	assert.NotNil(t, cars)
+	assert.EqualValues(t, "Mercedes-Benz A 200 d AMG Line", cars[0].Model)
 }
 
 func TestGetCarPower(t *testing.T) {
@@ -67,4 +68,45 @@ func TestGetCarPower(t *testing.T) {
 	}
 
 	assert.NotNil(t, cars)
+	assert.EqualValues(t, "2 143 cm3 • 136 cv", cars[0].Power)
+}
+
+func TestGetCarDetails(t *testing.T) {
+	// Arrange
+	server := localHtmlSetup()
+	s := New()
+
+	//Act
+	var cars []Car
+	s.StartCars(&cars, GetCarDetails)
+
+	//Assert
+	err := s.Visit(server.URL)
+	if err != nil {
+		panic(err)
+	}
+
+	assert.NotNil(t, cars)
+	assert.EqualValues(t, "92 580 km", cars[0].Mileage)
+	assert.EqualValues(t, "Diesel", cars[0].Fuel)
+	assert.EqualValues(t, "2017", cars[0].Year)
+}
+
+func TestGetCarPrice(t *testing.T) {
+	// Arrange
+	server := localHtmlSetup()
+	s := New()
+
+	//Act
+	var cars []Car
+	s.StartCars(&cars, GetCarPrice)
+
+	//Assert
+	err := s.Visit(server.URL)
+	if err != nil {
+		panic(err)
+	}
+
+	assert.NotNil(t, cars)
+	assert.EqualValues(t, "23 990", cars[0].Price)
 }
