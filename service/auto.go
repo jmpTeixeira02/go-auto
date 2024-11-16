8ae7a282eaf8fe1887bc8deb7156d4ab9d6e8509
Teixeira <51384667+jmpTeixeira02@users.noreply.github.com>
2024-11-16 00:23:09 +0000

    feat: parallel add file and send notification

--- service/auto.go
+++ service/auto.go
@@ -1,21 +1,35 @@
 package service
 
 import (
+	"context"
+	"errors"
 	"go-auto/data"
 	"go-auto/notifier"
 	"go-auto/scrapper"
 	dataService "go-auto/service/data"
+	"os/signal"
+	"sync"
+	"syscall"
 )
 
 type AutoService struct {
 	notifier notifier.Notifier
 	data     dataService.DataService
+
+	ctx   context.Context
+	errCh chan error
+	wg    sync.WaitGroup
 }
 
 func New(notifier notifier.Notifier, dataService dataService.DataService) AutoService {
+	ctx, _ := signal.NotifyContext(context.Background(), syscall.SIGINT)
+
 	return AutoService{
 		notifier: notifier,
 		data:     dataService,
+		ctx:      ctx,
+		errCh:    make(chan error),
+		wg:       sync.WaitGroup{},
 	}
 }
 
@@ -45,11 +59,35 @@ func (a *AutoService) GetCars(url string) error {
 		return err
 	}
 
-	for i := range new {
-		a.notifier.SendMessage(data.CarToString(new[i]))
-		a.data.AddCar(new[i])
-		// Add to db if new car was sucessufly saved
-	}
+	return a.addNewCars(new)
+}
 
-	return nil
+func (a *AutoService) addNewCars(new []data.Car) error {
+	nNew := len(new)
+	a.wg.Add(nNew)
+	a.errCh = make(chan error)
+	for i := range new {
+		go a.addCar(new[i])
+	}
+	a.wg.Wait()
+
+	var err error
+	for e := range a.errCh {
+		err = errors.Join(err, e)
+	}
+	return err
+}
+
+func (a *AutoService) addCar(car data.Car) {
+	err := a.notifier.SendMessage(data.CarToString(car))
+	if err != nil {
+		a.errCh <- err
+		return
+	}
+	_, err = a.data.AddCar(car)
+	if err != nil {
+		a.errCh <- err
+		return
+	}
+	a.wg.Done()
 }
